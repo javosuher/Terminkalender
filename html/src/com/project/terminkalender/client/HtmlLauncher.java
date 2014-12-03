8f8de600ea94bbccb568ddceba8c69b4bc41376b
Javier Osuna Herrera <javier.osunaherrera@alum.uca.es>
2014-12-03 13:29:09 +0100

    Añadiendo nueva estructura de libGDX

---
+++ html/src/com/project/terminkalender/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.project.terminkalender.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.project.terminkalender.Main;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new Main();
+        }
+}
